d377af7de00ec4b0fbde6f24917337fa1d664718
nina <67192642+quynhu-d@users.noreply.github.com>
2025-04-24 10:55:13 +0300

    dt and rf fix in classify.py

--- lib/classify.py
+++ lib/classify.py
@@ -20,7 +20,7 @@ def lsvc_classifier(X_train, y_train, X_test, y_test):
     auc_pr_svc_ = average_precision_score(y_test, b_pred_svc)
     
     return linear_svc, {
-        'train_acc': train_score, 'test_acc': test_score, 
+        'train_acc': train_score, 'test_acc': test_score,
         'auc_roc_test': auc_roc_svc_, 'auc_pr_test': auc_pr_svc_
     }
     
@@ -42,7 +42,7 @@ def svc_classifier(X_train, y_train, X_test, y_test):
     auc_pr_svc_ = average_precision_score(y_test, b_pred_svc)
     
     return best_model_svc, {
-        'train_acc': train_score, 'test_acc': test_score, 
+        'train_acc': train_score, 'test_acc': test_score,
         'auc_roc_test': auc_roc_svc_, 'auc_pr_test': auc_pr_svc_
     }
 
@@ -56,31 +56,18 @@ def decision_tree_classifier(
     X_train, y_train, X_test, y_test, 
     depth_grid=range(3, 16), samples_leaf_grid=range(1, 5), random_forest=False
 ):
-    models = {}
-    accuracy = {part: np.zeros((len(depth_grid), len(samples_leaf_grid))) for part in ['train', 'test']}
+    if random_forest:
+        model = RandomForestClassifier()
+    else:
+        model = DecisionTreeClassifier()
+    model_gscv = GridSearchCV(model, {"max_depth": depth_grid, "min_samples_leaf": samples_leaf_grid}, cv=10)    # chooses best by score estimate
+    model = model_gscv.fit(X_train, y_train)
 
-    for i, depth in tqdm(enumerate(depth_grid), total=len(depth_grid), leave=False):
-        for j, samples_leaf in enumerate(samples_leaf_grid):
-            if random_forest:
-                model = RandomForestClassifier(
-                    max_depth = depth, 
-                    min_samples_leaf = samples_leaf
-                ).fit(X_train, y_train)
-            else:
-                model = DecisionTreeClassifier(
-                    max_depth = depth, 
-                    min_samples_leaf = samples_leaf
-                ).fit(X_train, y_train)
-            pred_train = model.predict(X_train)
-            pred = model.predict(X_test)
-            accuracy['train'][i, j] = accuracy_score(y_train, pred_train) 
-            accuracy['test'][i, j] = accuracy_score(y_test, pred)
-            models[(depth, samples_leaf)] = model
-    for part in accuracy:
-        accuracy[part] = pd.DataFrame(accuracy[part])
-        accuracy[part].columns = samples_leaf_grid
-        accuracy[part].index = depth_grid
-    return models, accuracy
+    best_model = model.best_estimator_
+    train_score = best_model.score(X_train, y_train)
+    test_score = best_model.score(X_test, y_test)
+    
+    return best_model, {'train_acc': train_score, 'test_acc': test_score}
 
 def full_pipeline(df, method='wishart', features=None):
     df_train = df.query('part == \'train\'')
@@ -94,10 +81,10 @@ def full_pipeline(df, method='wishart', features=None):
         features = ['entropy', 'complexity']
     elif method == 'all':
         features = [
-            'entropy', 'complexity', 
-            'ws_min_icd', 'ws_max_icd', 'ws_mean_icd', 
-            'fws_min_icd', 'fws_max_icd', 'fws_mean_icd', 'noise', 'n_clusters', 
-            'km_min_icd', 'km_max_icd', 'km_mean_icd', 
+            'entropy', 'complexity',
+            'ws_min_icd', 'ws_max_icd', 'ws_mean_icd',
+            'fws_min_icd', 'fws_max_icd', 'fws_mean_icd', 'noise', 'n_clusters',
+            'km_min_icd', 'km_max_icd', 'km_mean_icd',
             'fcm_min_icd', 'fcm_max_icd', 'fcm_mean_icd'
         ]
     elif method == 'tda':
@@ -119,31 +106,21 @@ def full_pipeline(df, method='wishart', features=None):
     res_lsvc = lsvc_classifier(X_train.to_numpy(), y_train, X_test.to_numpy(), y_test)
     res_dt = decision_tree_classifier(X_train.to_numpy(), y_train, X_test.to_numpy(), y_test)
     res_rf = decision_tree_classifier(
-        X_train.to_numpy(), y_train, X_test.to_numpy(), y_test, 
+        X_train.to_numpy(), y_train, X_test.to_numpy(), y_test,
         samples_leaf_grid=range(1, 11),
         random_forest=True
     )
     
     res_clf = {}
 
-    res_rf_test = res_rf[1]['test']
-    res_rf_train = res_rf[1]['train']
-    best_idx = res_rf_test.to_numpy().argmax()
-    d, s = list(product(res_rf_test.index, res_rf_test.columns))[best_idx]
-    params = {'depth': d, 'samples_leaf': s}
-    train_acc = res_rf_train.to_numpy().flatten()[best_idx]
-    test_acc = res_rf_test.to_numpy().flatten()[best_idx]
-    res_clf['random_forest'] = {'train_acc': train_acc, 'test_acc': test_acc, 'params': params, 'model': res_rf[0][(d, s)]}
-
-    res_dt_test = res_dt[1]['test']
-    res_dt_train = res_dt[1]['train']
-    best_idx = res_dt_test.to_numpy().argmax()
-    d, s = list(product(res_dt_test.index, res_dt_test.columns))[best_idx]
-    params = {'depth': d, 'samples_leaf': s}
-    train_acc = res_dt_train.to_numpy().flatten()[best_idx]
-    test_acc = res_dt_test.to_numpy().flatten()[best_idx]
-    res_clf['decision_tree'] = {'train_acc': train_acc, 'test_acc': test_acc, 'params': params, 'model': res_rf[0][(d, s)]}
+    params_ = res_dt[0].get_params()
+    params_dt = {'depth': params_['max_depth'], 'samples_leaf': params_['min_samples_leaf']}
+    res_clf['decision_tree'] = {'params': params, 'model': res_dt[0]} | res_dt[1]   
     
+    params_ = res_rf[0].get_params()
+    params_rf = {'depth':params_['max_depth'], 'samples_leaf': params_['min_samples_leaf']}
+    res_clf['random_forest'] = {'params': params, 'model': res_rf[0]} | res_rf[1]    
+
     params = {'C': res_svc[0][1].C}
     res_clf['svc'] = {'params': params, 'model': res_svc[0]} | res_svc[1]
     res_clf['lsvc'] = {'model': res_lsvc[0]} | res_lsvc[1]
